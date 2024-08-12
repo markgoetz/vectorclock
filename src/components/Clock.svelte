398408872d444e5e9f087e31cadefcf87356a953
Mark Goetz <breakawayblue@gmail.com>
2024-08-11 20:09:52 -0400

    feat: Add running clock

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -1,7 +1,38 @@
-<div class="clock">12:57</div>
+<script lang="ts">
+  import { format } from 'date-fns/format';
+
+  let time = '00:00 AM';
+  let date = '00-00-0000';
+
+  function startClock(_node: HTMLElement) {
+    window.setInterval(() => {
+      const d = new Date();
+      time = format(d, 'hh:mm a');
+      date = format(d, 'MM-dd-yyyy');
+    }, 500);
+  };
+</script>
+
+<div class="layout" use:startClock>
+  <div class="time">{time}</div>
+  <div class="date">{date}</div>
+</div>
 
 <style>
-  .clock {
-    font-size: 250px;
+  .layout {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .time {
+    font-size: 200px;
+    line-height: 65%;
+  }
+  
+  .date {
+    font-size: 80px;
+    line-height: 65%;
   }
 </style>
